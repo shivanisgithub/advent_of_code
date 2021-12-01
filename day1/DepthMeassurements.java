8af5c0b96e76065d9d71b57964aeac876da90a4f
shivani <shivanisingh@live.de>
2021-12-01 21:05:02 +0100

    puzzle two solved as well

--- day1/DepthMeassurements.java
+++ day1/DepthMeassurements.java
@@ -1,9 +1,8 @@
-package d1p1;
+package day1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 class DepthMeassurements {
 
@@ -16,11 +15,11 @@ class DepthMeassurements {
             intArray = new int[0];
         };
 
-        System.out.println("meassurements increased: " + dm.GetMeasurementsLarger(intArray));
+        System.out.println("meassurements increased: " + dm.GetMeasurementsLarger(dm.ArrayOfSums(intArray)));
     }
 
     public static int[] processFile() throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File("d1p1/meassurements.txt"));
+        Scanner scanner = new Scanner(new File("day1/meassurements.txt"));
         List<Integer> meassurements = new ArrayList<Integer>();
         int i = 0;
         while(scanner.hasNextInt()){
@@ -43,5 +42,14 @@ class DepthMeassurements {
             }
         }
         return returnValue;
+    }
+
+    public static int[] ArrayOfSums(int[] depth) {
+        int l = depth.length;
+        int[] returnValues = new int[l-2];
+        for(int i = 0; i < l-2; i++) {
+            returnValues[i] = (depth[i]+depth[i+1]+depth[i+2]);
+        }
+        return returnValues;
     }
 }
